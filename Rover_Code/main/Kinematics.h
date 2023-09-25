52873020dd9a0791ca854592edc380d59c14dc69
Brittney Morales <brittneymrls@gmail.com>
2023-09-24 21:11:41 -0600

    added a copy of blink, kinematics and odometry

---
+++ Rover_Code/main/Kinematics.h
@@ -0,0 +1,54 @@
+#ifndef KINEMATICS_H
+#define KINEMATICS_H
+
+#define RPM_TO_RPS 1/60
+
+#include "math.h"
+
+typedef enum L_base {DIFFERENTIAL_DRIVE, SKID_STEER, MECANUM}base;
+
+typedef struct L_rpm
+{
+    float motor1;
+    float motor2;
+    float motor3;
+    float motor4;
+}R_rpm;
+
+typedef struct L_velocities
+{
+    float linear_x;
+    float linear_y;
+    float angular_z;
+}R_velocities;
+
+typedef struct L_pwm
+{
+    int motor1;
+    int motor2;
+    int motor3;
+    int motor4;
+}R_pwm;
+
+typedef struct L_Kinematics{
+    base base_platform_;
+    R_pwm pwm;
+    R_velocities velocities;
+    R_rpm rpm;
+    float max_rpm_;
+    float wheels_y_distance_;
+    float pwm_res_;
+    float wheel_circumference_;
+    int total_wheels_;
+}Kinematics;
+
+void Kinematics_Constructor(Kinematics * our_kino, base robot_base, int motor_max_rpm, float max_rpm_ratio,
+                   float motor_operating_voltage, float motor_power_max_voltage,
+                   float wheel_diameter, float wheels_y_distance);
+
+R_velocities getVelocities(Kinematics * our_kino, float rpm1, float rpm2, float rpm3, float rpm4);
+R_rpm getRPM(Kinematics * our_kino, float linear_x, float linear_y, float angular_z);
+float getMaxRPM(Kinematics * our_kino);
+R_rpm calculateRPM(Kinematics * our_kino, float linear_x, float linear_y, float angular_z);
+int getTotalWheels(base robot_base);
+#endif
---
+++ Rover_Code/main/Odometry.c
@@ -0,0 +1,74 @@
+#include "Odometry.h"
+#include "math.h"
+void Odometry_Constructor(Odometry * our_odo){
+    our_odo->x_pos_ = 0.0;
+    our_odo->y_pos_ = 0.0;
+    our_odo->heading_ = 0.0;
+    our_odo->odom_msg_.header.frame_id = micro_ros_string_utilities_set(our_odo->odom_msg_.header.frame_id, "odom");
+    our_odo->odom_msg_.child_frame_id = micro_ros_string_utilities_set(our_odo->odom_msg_.child_frame_id, "base_footprint");
+}
+
+void update(Odometry * our_odo, float vel_dt, float linear_vel_x, float linear_vel_y, float angular_vel_z){
+    float delta_heading = angular_vel_z * vel_dt; //radians
+    float cos_h = cos(our_odo->heading_);
+    float sin_h = sin(our_odo->heading_);
+    float delta_x = (linear_vel_x * cos_h - linear_vel_y * sin_h) * vel_dt; //m
+    float delta_y = (linear_vel_x * sin_h + linear_vel_y * cos_h) * vel_dt; //m
+
+    //calculate current position of the robot
+    our_odo->x_pos_ += delta_x;
+    our_odo->y_pos_ += delta_y;
+    our_odo->heading_ += delta_heading;
+
+    //calculate robot's heading in quaternion angle
+    //ROS has a function to calculate yaw in quaternion angle
+    float q[4];
+    euler_to_quat(0, 0, our_odo->heading_, q);
+
+    //robot's position in x,y, and z
+    our_odo->odom_msg_.pose.pose.position.x = our_odo->x_pos_;
+    our_odo->odom_msg_.pose.pose.position.y = our_odo->y_pos_;
+    our_odo->odom_msg_.pose.pose.position.z = 0.0;
+
+    //robot's heading in quaternion
+    our_odo->odom_msg_.pose.pose.orientation.x = (double) q[1];
+    our_odo->odom_msg_.pose.pose.orientation.y = (double) q[2];
+    our_odo->odom_msg_.pose.pose.orientation.z = (double) q[3];
+    our_odo->odom_msg_.pose.pose.orientation.w = (double) q[0];
+
+    our_odo->odom_msg_.pose.covariance[0] = 0.001;
+    our_odo->odom_msg_.pose.covariance[7] = 0.001;
+    our_odo->odom_msg_.pose.covariance[35] = 0.001;
+
+    //linear speed from encoders
+    our_odo->odom_msg_.twist.twist.linear.x = linear_vel_x;
+    our_odo->odom_msg_.twist.twist.linear.y = linear_vel_y;
+    our_odo->odom_msg_.twist.twist.linear.z = 0.0;
+
+    //angular speed from encoders
+    our_odo->odom_msg_.twist.twist.angular.x = 0.0;
+    our_odo->odom_msg_.twist.twist.angular.y = 0.0;
+    our_odo->odom_msg_.twist.twist.angular.z = angular_vel_z;
+
+    our_odo->odom_msg_.twist.covariance[0] = 0.0001;
+    our_odo->odom_msg_.twist.covariance[7] = 0.0001;
+    our_odo->odom_msg_.twist.covariance[35] = 0.0001;
+}
+
+nav_msgs__msg__Odometry getData(Odometry * our_odo){
+    return our_odo->odom_msg_;
+}
+
+void euler_to_quat(float roll, float pitch, float yaw, float* q){
+    float cy = cos(yaw * 0.5);
+    float sy = sin(yaw * 0.5);
+    float cp = cos(pitch * 0.5);
+    float sp = sin(pitch * 0.5);
+    float cr = cos(roll * 0.5);
+    float sr = sin(roll * 0.5);
+
+    q[0] = cy * cp * cr + sy * sp * sr;
+    q[1] = cy * cp * sr - sy * sp * cr;
+    q[2] = sy * cp * sr + cy * sp * cr;
+    q[3] = sy * cp * cr - cy * sp * sr;
+}
