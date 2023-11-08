4ab9fa8e7fad604034a6a22787414bbd6b934fdc
Misael Nava <misaelnava812@gmail.com>
2023-11-08 10:23:20 -0700

    Initial Code for Battery Sensing and LED to signal

---
+++ Individual_Component_Code/Battery_sensing/main/blink.h
@@ -0,0 +1,16 @@
+#ifndef OUR_LED_H
+#define OUR_LED_H
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO 2 // GPIO pin for the LED (change to your desired GPIO pin)
+
+void led_setup(void);
+void led_blink(int n_times);
+void led_turn_on(void);
+
+#endif
