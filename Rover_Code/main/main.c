0e52fe4fede8a9469c2e5d55feea68f4e3d432ad
Brittney Morales <brittneymrls@gmail.com>
2023-11-17 19:16:55 -0700

    updated integration of lidar to main
    updated poll_lidar and lidar_setup method
    created the publisher and lidar_msg in main instead in lidar.c
    added timer_callback and mircro_ros_task method to main.c (for debugging purposes)

--- Rover_Code/main/main.c
+++ Rover_Code/main/main.c
@@ -51,8 +51,8 @@
 #include "our_lidar.h"
 
 
-#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){rclErrorLoop();}}
-#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+// #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){rclErrorLoop();}}
+// #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
 
 #define EXECUTE_EVERY_N_MS(MS, X)  do { \
   static volatile int64_t init = -1; \
@@ -80,15 +80,15 @@
 
 rcl_publisher_t odom_publisher;
 rcl_publisher_t imu_publisher;
-// rcl_publisher_t lid_publisher; //??????
+rcl_publisher_t lidar_publisher; //??????
 rcl_subscription_t twist_subscriber;
 // publisher for lidar is in our_lidar.c file called lidar_publisher
 
 nav_msgs__msg__Odometry odom_msg;
 sensor_msgs__msg__Imu imu_msg;
-// sensor_msgs__msg__LaserScan lid_msg;
+sensor_msgs__msg__LaserScan lidar_msg_;
 geometry_msgs__msg__Twist twist_msg;
-//lidar message is in our_lidar.c file called lidar_msg_
+// lidar message is in our_lidar.c file called lidar_msg_
 
 rclc_executor_t executor;
 rclc_support_t support;
@@ -174,6 +174,98 @@ void micro_ros_main_loop(void * arg){
 }
 
 
+void timer_callback_lidar(rcl_timer_t * timer, int64_t last_call_time)
+{
+	RCLC_UNUSED(last_call_time);
+	if (timer != NULL) {
+		printf("Publishing angle increment: %f\n", (float) lidar_msg_.angle_increment);
+		printf("Publishing angle min: %f\n", (float) lidar_msg_.angle_min);
+		printf("Publishing angle max: %f\n", (float) lidar_msg_.angle_max);
+		printf("Publishing range min: %f\n", (float) lidar_msg_.range_min);
+		printf("Publishing range max: %f\n", (float) lidar_msg_.range_max);
+
+        for(int i = 0; i<360 ; i++)
+        {
+		    printf("Publishing range %d: %f\n", i, (float) lidar_msg_.ranges.data[i]);
+		    printf("Publishing range %d: %f\n", i, (float) lidar_msg_.intensities.data[i]);
+        }
+
+		RCSOFTCHECK(rcl_publish(&lidar_publisher, &lidar_msg_, NULL));
+	}
+}
+
+
+void micro_ros_task_lidar ( void * arg){
+    ESP_LOGI(TAG_LIDAR, "Inside micro ros for lidar");
+    /*
+    //-------------------- setup of node, and publisher
+    rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
+
+	rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+	RCCHECK(rcl_init_options_init(&init_options, allocator));
+
+#ifdef CONFIG_MICRO_ROS_ESP_XRCE_DDS_MIDDLEWARE
+	rmw_init_options_t* rmw_options = rcl_init_options_get_rmw_init_options(&init_options);
+
+	// Static Agent IP and port can be used instead of autodisvery.
+	RCCHECK(rmw_uros_options_set_udp_address(CONFIG_MICRO_ROS_AGENT_IP, CONFIG_MICRO_ROS_AGENT_PORT, rmw_options));
+	//RCCHECK(rmw_uros_discover_agent(rmw_options));
+#endif
+    ESP_LOGI(TAG_LIDAR, "create init_options");
+
+	// create init_options
+	//RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
+    bool value_rclc = false;
+    if (RCL_RET_ERROR == rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator)){
+        value_rclc = true;
+    }
+    ESP_LOGI(TAG_LIDAR, "result for rclc: %d ", value_rclc); //next best thing to do to debug is cross-reference with linorobot and main.c
+	RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
+    */
+    // ESP_LOGI(TAG_LIDAR, "create node"); //next best thing to do to debug is cross-reference with linorobot and main.c
+
+	// // create node
+	// rcl_node_t node;
+	// RCCHECK(rclc_node_init_default(&node, "/scan/laser_scan", "", &support));
+
+	// create publisher
+	RCCHECK(rclc_publisher_init_default(
+		&lidar_publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, LaserScan),
+		"scan"));
+
+    // // create timer,
+	// rcl_timer_t timer;
+	// const unsigned int timer_timeout = 1000;
+	// RCCHECK(rclc_timer_init_default(
+	// 	&timer,
+	// 	&support,
+	// 	RCL_MS_TO_NS(timer_timeout),
+	// 	timer_callback_lidar));
+
+	// // create executor
+	// rclc_executor_t executor;
+	// RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+	// RCCHECK(rclc_executor_add_timer(&executor, &timer));
+    //this is where I stopped ??????????? keep debugging ------------------------
+
+    while(1) {
+
+        poll_lidar(&lidar_msg_);
+
+        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+        vTaskDelay(100); //this is how often it is getting called
+    }
+
+    // free resources
+	RCCHECK(rcl_publisher_fini(&lidar_publisher, &node));
+	RCCHECK(rcl_node_fini(&node));
+
+  	vTaskDelete(NULL);
+}
+
 void app_main(void)
 {
     //set up led
@@ -199,7 +291,7 @@ void app_main(void)
     setup_imu();
 
     //Lidar setup
-    lidar_setup();
+    lidar_setup(&lidar_msg_);
 
     #if defined(CONFIG_MICRO_ROS_ESP_NETIF_WLAN) || defined(CONFIG_MICRO_ROS_ESP_NETIF_ENET)
     //ESP_LOGI(TAG_ERROR, "transport");
@@ -209,15 +301,12 @@ void app_main(void)
 
     state = WAITING_AGENT;
 
+    ESP_LOGI(TAG_LIDAR, "micros ros main for uros_task");
     xTaskCreate(micro_ros_main_loop, "uros_task", 3000, NULL, 5, NULL);
 
     //lidar task (update data for 16000(stack) and 5(priority) should I change this?)
-    xTaskCreate(micro_ros_task,
-        "uros_task",
-        16000,
-        NULL,
-        5,
-        NULL);
+    ESP_LOGI(TAG_LIDAR, "micro ros for lidar");
+    xTaskCreate(micro_ros_task_lidar, "uros_task", 16000, NULL, 5, NULL);
 }
 
 void controlCallback(rcl_timer_t * timer, int64_t last_call_time) 
