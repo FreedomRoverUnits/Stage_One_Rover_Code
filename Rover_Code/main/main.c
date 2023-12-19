348c76e9a29660b8367afd4025986e1a964c7d48
Brittney Morales <brittneymrls@gmail.com>
2023-12-18 21:07:20 -0700

    Added apachy license to each file.

--- Rover_Code/main/main.c
+++ Rover_Code/main/main.c
@@ -1,5 +1,3 @@
-// Copyright (c) 2021 Juan Miguel Jimeno
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -11,8 +9,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//#include <Arduino.h>
-//#include <micro_ros_platformio.h>
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
